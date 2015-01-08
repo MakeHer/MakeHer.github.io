e0345ce78770b4f55f4e042e53b6176906fb51c3
CallaK <calla.klafas@gmail.com>
2015-01-08 15:09:38 +1100

    Fixed logo to scroll effect

--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
-	var element = $('#fixy');
-	var navTopPos = $(element).offset().top;
+	var elementBar = $('#fixy');
+	var elementLogo = $('#logo');
+	var navTopPos = $(elementBar).offset().top;
 
 	$(window).scroll(function(event){
 		var y = $(this).scrollTop();
 
 		// if y position is greater than the navtop position, then need to make it fixed position and top=0
 			if (y>=navTopPos){
-				element.css({"position" : "fixed", "top" : "0", "bottom" : ""});
+				elementBar.css({"position" : "fixed", "top" : "0", "bottom" : ""});
+				elementLogo.css({"position" : "fixed","top":"-200px","bottom":""})
 			}
-			else{
-				element.css({"position" : "absolute", "bottom" : "0", "top" : ""});
+			else{	
+				elementBar.css({"position" : "absolute", "bottom" : "0", "top" : ""})
+				elementLogo.css({"position" : "absolute", "bottom" : "0", "top" : ""});
 			}
 
 		// else need to convert it back to absolute and bottom -0
